734ec865880b867a0e728a9dcd53046d64973dc2
Joren Van Onder <joren.vanonder@gmail.com>
2018-06-16 12:28:58 -0700

    Handle cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
+extern crate clap;
 extern crate recaman_svg;
 
+use clap::{App, Arg};
 use recaman_svg::*;
 
 pub fn main() {
-    let sequence = recaman_sequence(100);
+    let matches = App::new("Recamán SVG generator")
+        .version("1.0")
+        .author("Joren Van Onder <joren.vanonder@gmail.com>")
+        .about("TODO")
+        .arg(
+            Arg::with_name("amount")
+                .short("n")
+                .long("amount")
+                .default_value("20")
+                .help("Amount of Recamán points to generate.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("rotation")
+                .short("r")
+                .long("rotation")
+                .default_value("0")
+                .help("Rotate the image by <rotation> degrees.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Write SVG to OUTPUT.")
+                .required(true),
+        )
+        .get_matches();
+
+    let amount: u32 = matches
+        .value_of("amount")
+        .unwrap()
+        .parse()
+        .expect("Not a valid integer.");
+    let file = matches.value_of("OUTPUT").unwrap();
+
+    let sequence = recaman_sequence(amount);
     let svg = generate_svg_document(&sequence);
-    write_svg_document(svg, "/tmp/image.svg");
+    write_svg_document(svg, file);
 }
