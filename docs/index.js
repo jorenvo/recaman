04ff53ab19812271e30657816c54b1dd3f86d7b4
Joren Van Onder <joren.vanonder@gmail.com>
2018-06-17 17:18:57 -0700

    Hardcode path length in CSS
    Not ideal but this example most likely won't change and this works
    across all browsers.

--- docs/index.js
+++
@@ -1,7 +0,0 @@
-function set_path_length () {
-    let path = document.querySelector('.recaman-svg-path');
-    let length = path.getTotalLength();
-
-    path.style['stroke-dasharray'] = length;
-    path.style['stroke-dashoffset'] = length;
-}
